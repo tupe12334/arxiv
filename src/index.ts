280d2aba7ee52a22fc1a7f640d57137b62b103b4
tupe12334 <ofek.gabay.he@gmail.com>
2025-01-11 22:46:13 +0200

    Add axios dependency and implement ArxivClient class

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @author @tupe12334
+ * @see https://info.arxiv.org/help/api/user-manual.html
+ */
+import axios from "axios";
+
+class ArxivClient {
+  private baseUrl = "http://export.arxiv.org/api/query";
+  constructor() {}
+}
+
+export const arxiv = new ArxivClient();
