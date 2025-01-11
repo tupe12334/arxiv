e4cecb5437b0b5b519a51c89cddaab7d7de0cbbe
tupe12334 <ofek.gabay.he@gmail.com>
2025-01-11 23:23:14 +0200

    Add Jest configuration and update package dependencies

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+};
